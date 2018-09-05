89e632a36020415dc545e8b7ba46cb09ae6b1c28
Pim Hoefmans <pimhoefmans@hotmail.com>
2018-09-05 16:04:52 +0200

    nu meer pizza met putput en post

--- Pizzapii3.py
+++ Pizzapii3.py
@@ -3,11 +3,20 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 pizzaDB = [
-    {'name':'magarita','desc':'normie'},
-    {'name':'hawaii', 'desc':'lit'},
-    {'name': 'salami', 'desc':'salami more like sla me'}
+    {'name':'magarita','ingr': 'normal', 'prijs': '6,50'},
+    {'name':'hawaii', 'ingr': 'pineapple', 'prijs': 'your soul'},
+    {'name': 'salami', 'ingr': 'salami xp', 'prijs': 'trie fiddy'}
 ]
 
+@app.route("/", methods= ['POST'])
+def addOnePizza():
+    pizza = {'name' : request.json['name']}
+    pizzaDB.append(pizza)
+    return jsonify({'pizzaDB': pizzaDB})
+
+
+
+"""
 @app.route("/<string:name>", methods=['GET'])
 def getOnePizza():
     resultPizza = []
@@ -19,6 +28,16 @@ def getOnePizza():
             print("ugu")
 
     return jsonify({'pizzaDB':result:pizzaDB})
+"""
+
+@app.route("/<string:name>", methods=['PUT'])
+def putPizza(name):
+    resultPizza = []
+    for pizza in pizzaDB:
+        if pizza['name'] == name:
+            resultPizza.append(pizza)
+    resultPizza[0]['name'] = request.json['name']
+    return jsonify({'pizzaDB': pizzaDB})
 
 if __name__ == "__main__":
     app.run()
